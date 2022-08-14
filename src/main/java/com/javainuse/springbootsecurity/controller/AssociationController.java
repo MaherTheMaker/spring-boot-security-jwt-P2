c79e8fb6cdd3bbfd28912f53c216bab91d4ca88d
MaherTheMaker <maher.serawan.1998@gmail.com>
2022-08-15 00:55:19 +0300

    qusai

--- src/main/java/com/javainuse/springbootsecurity/controller/AssociationController.java
+++ src/main/java/com/javainuse/springbootsecurity/controller/AssociationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/ass")
 public class AssociationController {
 
+    // testing quasi
     @Autowired
     AssociationService associationService;
 
